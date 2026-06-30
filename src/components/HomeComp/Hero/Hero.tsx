b7025e0480242f044adf1fda413c16f181dc36f5
active64web <active64web@gmail.com>
2026-06-30 13:46:09 +0300

    change hero section

--- src/components/HomeComp/Hero/Hero.tsx
+++ src/components/HomeComp/Hero/Hero.tsx
@@ -1,35 +1,62 @@
+"use client";
+
 import "./Hero.scss";
 import Image from "next/image";
 import { Link } from "@/i18n/navigation";
 import { useTranslations } from "next-intl";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, EffectFade, Pagination } from "swiper/modules";
+
+const STATIC_SLIDES = [
+    { id: 1, image: "/images/image-welcome.jpg" },
+    { id: 2, image: "/images/image-welcome.jpg" },
+    { id: 3, image: "/images/image-welcome.jpg" },
+];
 
 const Hero = () => {
     const t = useTranslations("welcome");
 
     return (
         <div className="hero">
-            <Image
-                src="/images/image-welcome.jpg"
-                alt="welcome-active4web"
-                fill
-                priority
-                style={{ objectFit: 'cover' }}
-            />
+            <Swiper
+                modules={[Autoplay, EffectFade, Pagination]}
+                effect={"fade"}
+                autoplay={{ delay: 5000, disableOnInteraction: false }}
+                pagination={{ clickable: true }}
+                loop={true}
+                className="hero-swiper"
+            >
+                {STATIC_SLIDES.map((slide) => (
+                    <SwiperSlide key={slide.id} className="hero-slide">
+                        <div className="image-wrapper">
+                            <Image
+                                src={slide.image}
+                                alt="welcome-active4web"
+                                fill
+                                priority
+                                style={{ objectFit: 'cover' }}
+                            />
+                        </div>
 
-            <div className="box-text">
-                <p>
-                    <span>{"//"}</span>
-                    <span>{t("sub")}</span>
-                </p>
 
-                <h1>{t("title")}</h1>
+                        <div className="box-text">
+                            <p>
+                                <span>{"//"}</span>
+                                <span>{t("sub")}</span>
+                            </p>
 
-                <p>{t("desc")}</p>
+                            <h1>{t("title")}</h1>
 
-                <Link href="/services">
-                    {t("more")}
-                </Link>
-            </div>
+                            <p>{t("desc")}</p>
+
+                            <Link href="/services">
+                                {t("more")}
+                            </Link>
+                        </div>
+                    </SwiperSlide>
+                ))}
+            </Swiper>
         </div>
     );
 }
